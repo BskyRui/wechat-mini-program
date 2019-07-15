cdb8c57a221c9bf927d221fb97f9bfae8773f365
LiRui <bskyrui@msn.cn>
2019-07-15 15:09:50 +0800

    add bindtap event

--- pages/demo/demo.js
+++ pages/demo/demo.js
@@ -5,62 +5,16 @@ Page({
    * Page initial data
    */
   data: {
-
+    text: "pages/demo/demo.wxml?from=data"
+  },
+  
+  to: function() {
+    // console.log('hehehe');
+    this.calc();
+    
   },
 
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
+  calc: function() {
+    console.log('heheheha');
   }
 })
